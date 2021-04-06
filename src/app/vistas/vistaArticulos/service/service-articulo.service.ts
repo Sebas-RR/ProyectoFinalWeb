0cc352f4e98fc3f54f984f11d6d659f2175ee509
Karen Benedetti <kbenedetti9@gmail.com>
2021-04-06 11:24:31 -0500

    Creando servicio articulo

--- src/app/vistas/vistaArticulos/service/service-articulo.service.ts
+++ src/app/vistas/vistaArticulos/service/service-articulo.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceArticuloService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public listarArticulos():Observable<any>{
+    return this.http.get('http://localhost:9898/api/articulo/listar-articulos');
+  }
 }
