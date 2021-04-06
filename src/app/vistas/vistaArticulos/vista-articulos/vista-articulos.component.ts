9bb9e8a4ea0e71a662cc8c02ae5cb3260557a153
Karen Benedetti <kbenedetti9@gmail.com>
2021-04-06 11:36:03 -0500

    Creando metodo para obtener articulos

--- src/app/vistas/vistaArticulos/vista-articulos/vista-articulos.component.ts
+++ src/app/vistas/vistaArticulos/vista-articulos/vista-articulos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticuloModel } from 'src/app/models/articulo-model';
+import { ServiceArticuloService } from '../service/service-articulo.service';
 
 @Component({
   selector: 'app-vista-articulos',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VistaArticulosComponent implements OnInit {
 
-  constructor() { }
+  listaArticulos: Array<ArticuloModel> = [];
+
+  constructor(private service: ServiceArticuloService) { }
 
   ngOnInit(): void {
+    this.obtenerArticulos();
   }
 
+  inicializar() {
+
+  }
+
+  private obtenerArticulos() {
+    this.service.listarArticulos().subscribe(res => {
+      this.listaArticulos = res;
+    })
+  }
 }
