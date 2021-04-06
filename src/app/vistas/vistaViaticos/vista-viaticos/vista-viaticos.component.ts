55b16043b4a2f7dc5dab30be4434e09fd1eed14e
Your Name <you@example.com>
2021-04-06 18:17:13 -0500

    Vista viaticos

--- src/app/vistas/vistaViaticos/vista-viaticos/vista-viaticos.component.ts
+++ src/app/vistas/vistaViaticos/vista-viaticos/vista-viaticos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RespuestaViaticos, ViaticoModel } from 'src/app/models/viatico-models';
+import { ServiceViaticosService } from '../service/service-viaticos.service';
 
 @Component({
   selector: 'app-vista-viaticos',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VistaViaticosComponent implements OnInit {
 
-  constructor() { }
+  public viatico: ViaticoModel[] = [];
+  public fechaIni: Date;
+  public fechaFin: Date | undefined ;
+
+  constructor(private viaticosService: ServiceViaticosService) { }
 
   ngOnInit(): void {
+  }
+
+
+  public getViaticosService(fechaIni: Date, fechaFin: Date): void {
+    this.viaticosService.getViaticosFechas(fechaIni, fechaFin).subscribe(
+      (res: RespuestaViaticos) => {
+        this.viatico = res.objeto_respuesta;
+      },
+      (err) => {
+        console.error('Se ha producido un error: ', err);
+      }
+    );
   }
 
 }
