96bb25ccccf82528fa99b4575a2bbd0cdfb4e500
Karen Benedetti <kbenedetti9@gmail.com>
2021-04-06 17:12:21 -0500

    Creando metodo para obtener articulo por fecha

--- src/app/vistas/vistaArticulos/vista-articulos/vista-articulos.component.ts
+++ src/app/vistas/vistaArticulos/vista-articulos/vista-articulos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ArticuloModel } from 'src/app/models/articulo-model';
+import { ArticuloModel, RespuestaArticulo } from 'src/app/models/articulo-model';
 import { ServiceArticuloService } from '../service/service-articulo.service';
 
 @Component({
@@ -10,20 +10,25 @@ import { ServiceArticuloService } from '../service/service-articulo.service';
 export class VistaArticulosComponent implements OnInit {
 
   listaArticulos: Array<ArticuloModel> = [];
+  fechaInicio: String = '0000-00-00';
+  fechaFin: String = '0000-00-00';
 
   constructor(private service: ServiceArticuloService) { }
 
   ngOnInit(): void {
     this.obtenerArticulos();
-  }
-
-  inicializar() {
-
+    this.listarFecha("2021-03-28", "2021-04-07");
   }
 
   private obtenerArticulos() {
-    this.service.listarArticulos().subscribe(res => {
-      this.listaArticulos = res;
+    this.service.listarArticulos().subscribe((res: RespuestaArticulo) => {
+      this.listaArticulos = res.objecto_respuesta;
+    })
+  }
+
+  public listarFecha(fechaInicio: string, fechaFin: string): void {
+    this.service.listarFechas(fechaInicio, fechaFin).subscribe((res: RespuestaArticulo) => {
+      this.listaArticulos = res.objecto_respuesta;
     })
   }
 }
