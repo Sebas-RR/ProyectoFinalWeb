744a8b79c0bbfa8176ae0c95bdb239b9485e6214
Karen Benedetti <kbenedetti9@gmail.com>
2021-04-06 17:09:37 -0500

    Modificando servicio de articulo

--- src/app/vistas/vistaArticulos/service/service-articulo.service.ts
+++ src/app/vistas/vistaArticulos/service/service-articulo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,11 @@ export class ServiceArticuloService {
 
   constructor(private http: HttpClient) { }
 
-  public listarArticulos():Observable<any>{
+  public listarArticulos(): Observable<any> {
     return this.http.get('http://localhost:9898/api/articulo/listar-articulos');
+  }
+
+  public listarFechas(fechaInicio: string, fechaFin: string): Observable<any> {
+    return this.http.get(`http://localhost:9898/api/articulo/listar-fechas?fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`);
   }
 }
