cb3a15b7fdd52bed007d68dda6f2fa488048f405
AlejoProyectoJava <alejorestre004@gmail.com>
2021-04-07 22:07:57 -0500

    merge with Alejo

--- src/app/vistas/vistaVentas/service/service-ventas.service.ts
+++ src/app/vistas/vistaVentas/service/service-ventas.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {  Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceVentasService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public obtenerventas():Observable<any> {
+    return this.http.get('http://localhost:9898/api/venta/get-ventas');
+  }
+
+  public listarfechas(fecha1:String , fecha2: String): Observable<any> {
+    return this.http.get(`http://localhost:9898/api/venta/buscar-venta-fecha?fecha1=${fecha1}&fecha2=${fecha2}`)
+   }
 }
--- src/app/vistas/vistaVentas/vista-ventas/vista-ventas.component.ts
+++ src/app/vistas/vistaVentas/vista-ventas/vista-ventas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { VentaModel, RespuestaVenta} from 'src/app/models/venta-model';
+import { ServiceVentasService } from '../service/service-ventas.service';
 
 @Component({
   selector: 'app-vista-ventas',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VistaVentasComponent implements OnInit {
 
-  constructor() { }
+  listaventas: Array<VentaModel> = [];
+  listaventasfecha: Array<VentaModel> = [];
+  fecha1: String = '0000-00-00';
+  fecha2: String = '0000-00-00';
+  constructor(
+    private service:ServiceVentasService
+  ) { }
 
   ngOnInit(): void {
+    //this.obtenerventas();
+    //this.inicializar();
+    //this.listarfecha("2021-01-01","2021-03-03");
   }
+
+
+  private obtenerventas() {
+    this.service.obtenerventas().subscribe((res: RespuestaVenta) =>{
+      this.listaventas = res.objecto_respuesta;
+    })
+   }
+
+   public listarfecha(fecha1:String , fecha2:String): void {
+     this.service.listarfechas(fecha1,fecha2).subscribe((res:RespuestaVenta )=>{
+       this.listaventasfecha = res.objecto_respuesta;
+     })
+   }
+
+   inicializar() {}
 
 }
